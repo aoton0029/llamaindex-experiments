0fb6070893f78ee1595852da3b567fff1efb4c14
notoa <38271764+aoton0029@users.noreply.github.com>
2025-12-02 16:52:42 +0900

    Delete src/utils.py

--- src/utils.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-def yaml_to_dict(yaml_str:str) -> dict:
-    try:
-        data = yaml.safe_load(yaml_str)
-        return data if isinstance(data, dict) else (data or {})
-    except yaml.YAMLError as e:
-        raise ValueError(e)
